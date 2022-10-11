2c71691eb3d869a7f3114ed8ff94ad3faa7229f2
Peter Shen <xianpeng.shen@gmail.com>
2022-10-11 15:20:52 +0800

    test: Update cpp-linter option (#4)
    * test: Update cpp-linter option
    
    * feat: Only update on master branch
    
    * fix: Format code with cpp-linter
    
    Co-authored-by: shenxianpeng <3353385+shenxianpeng@users.noreply.github.com>

--- foo.c
+++ foo.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
-void foo(int num)
-{
-    if (num == 1) {
-        printf("when num is equal to 1...\n");
-    } else if (num == 2){
-        printf("when num is equal to 2...\n");
-    } else {
-        printf("when num is equal to %d...\n", num);
-    }
+void foo(int num) {
+  if (num == 1) {
+    printf("when num is equal to 1...\n");
+  } else if (num == 2) {
+    printf("when num is equal to 2...\n");
+  } else {
+    printf("when num is equal to %d...\n", num);
+  }
 }
--- main.c
+++ main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
+
 #include "foo.h"
- 
-int main(void)
-{
-    printf("Start calling foo() ...\n");
-    foo(1);
-    foo(2);
-    return 0;
+
+int main(void) {
+  printf("Start calling foo() ...\n");
+  foo(1);
+  foo(2);
+  return 0;
 }
